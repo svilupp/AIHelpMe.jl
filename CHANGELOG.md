10b5d01da5372d5f94d79e37f92a2bdb95caca23
J S <49557684+svilupp@users.noreply.github.com>
2024-05-27 20:00:08 +0100

    fix build_index, add tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
+## [0.1.1]
+
+### Fixed
+- Fixed a bug in `build_index` where imports were missing and keywords were not passed properly in all scenarios.
+
 ## [0.1.0]
 
 ### Added
--- test/preparation.jl
+++ test/preparation.jl
@@ -1,4 +1,4 @@
-using AIHelpMe: docextract
+using AIHelpMe: docextract, build_index
 
 # create an empty module
 module ABC123
@@ -23,4 +23,73 @@ end
     @test occursin("some text", all_docs[2])
     @test length(all_sources) == 2
     @test occursin("ABC1234", all_sources[2])
+end
+
+@testset "build_index" begin
+    # test with a mock server
+    PORT = rand(9000:31000)
+    PT.register_model!(; name = "mock-emb", schema = PT.CustomOpenAISchema())
+    PT.register_model!(; name = "mock-meta", schema = PT.CustomOpenAISchema())
+    PT.register_model!(; name = "mock-gen", schema = PT.CustomOpenAISchema())
+
+    echo_server = HTTP.serve!(PORT; verbose = -1) do req
+        content = JSON3.read(req.body)
+
+        if content[:model] == "mock-gen"
+            user_msg = last(content[:messages])
+            response = Dict(
+                :choices => [
+                    Dict(:message => user_msg, :finish_reason => "stop")
+                ],
+                :model => content[:model],
+                :usage => Dict(:total_tokens => length(user_msg[:content]),
+                    :prompt_tokens => length(user_msg[:content]),
+                    :completion_tokens => 0))
+        elseif content[:model] == "mock-emb"
+            response = Dict(
+                :data => [Dict(:embedding => ones(Float32, 1536))
+                          for i in 1:length(content[:input])],
+                :usage => Dict(:total_tokens => length(content[:input]),
+                    :prompt_tokens => length(content[:input]),
+                    :completion_tokens => 0))
+        elseif content[:model] == "mock-meta"
+            user_msg = last(content[:messages])
+            response = Dict(
+                :choices => [
+                    Dict(:finish_reason => "stop",
+                    :message => Dict(:tool_calls => [
+                        Dict(:function => Dict(:arguments => JSON3.write(MaybeTags([
+                        Tag("yes", "category")
+                    ]))))]))],
+                :model => content[:model],
+                :usage => Dict(:total_tokens => length(user_msg[:content]),
+                    :prompt_tokens => length(user_msg[:content]),
+                    :completion_tokens => 0))
+        else
+            @info content
+        end
+        return HTTP.Response(200, JSON3.write(response))
+    end
+
+    # One module
+    index = build_index(AIHelpMe; verbose = 2, embedder_kwargs = (; model = "mock-emb"),
+        tagger_kwargs = (; model = "mock-meta"), api_kwargs = (;
+            url = "http://localhost:$(PORT)"))
+    @test index.embeddings == ones(Bool, 1024, length(index.chunks))
+    @test all(x -> occursin("AIHelpMe", x), index.sources)
+    @test index.tags == nothing
+    @test index.tags_vocab == nothing
+
+    # Many modules
+    index = build_index(
+        [AIHelpMe, Test]; verbose = 2, embedder_kwargs = (; model = "mock-emb"),
+        tagger_kwargs = (; model = "mock-meta"), api_kwargs = (;
+            url = "http://localhost:$(PORT)"))
+    @test index.embeddings == ones(Bool, 1024, length(index.chunks))
+    @test all(x -> occursin("AIHelpMe", x) || occursin("Test", x), index.sources)
+    @test index.tags == nothing
+    @test index.tags_vocab == nothing
+
+    # clean up
+    close(echo_server)
 end
